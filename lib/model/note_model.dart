77a0da28b875b5565b701f1fd8398d56faa4c360
ibrahimmansy12 <159187714+ibrahimmansy12@users.noreply.github.com>
2024-02-08 13:12:30 +0200

    create node model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,13 @@
+class Notemodel {
+  final String title;
+  final String suptitle;
+  final String date;
+  final int color;
+
+  Notemodel(
+      {required this.title,
+      required this.suptitle,
+      required this.date,
+      required this.color});
+      
+}
